37aa634acd86946a1e6324511646878689f6dfc8
Aritra1704 <aritra1704@gmail.com>
2017-02-19 12:11:47 +0530

    Updates: Modified test case.

--- app/src/androidTest/java/com/arpaul/speechtotext/common/AppConstantTest.java
+++ app/src/androidTest/java/com/arpaul/speechtotext/common/AppConstantTest.java
@@ -5,6 +5,7 @@ import com.arpaul.speechtotext.dataobject.PhraseDO;
 import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import java.util.ArrayList;
 
@@ -27,14 +28,39 @@ public class AppConstantTest {
                 objPhraseDO.PhraseIteration = 1;
             arrPhrases.add(objPhraseDO);
         }
+
+
+
         ArrayList<String> arrTest = new ArrayList<>();
 //        arrTest.add("hello world 20");
+//        arrTest.add("hello world twenty");
 //        arrTest.add("hello world XX");
-        arrTest.add("hello world twenty");
-//        arrTest.add("helloworld xx");
+        arrTest.add("helloworld xx");
 //        arrTest.add("helloworld 20");
-        Assert.assertEquals(AppConstant.checkPhrase(arrTest) , arrPhrases);
+//        Assert.assertEquals(AppConstant.checkPhrase(arrTest) , arrPhrases);
+        checkPhraseObject(AppConstant.checkPhrase(arrTest) , arrPhrases);
 //        AppConstant.checkPhrase(arrTest).equals(arrPhrases);
+    }
+
+    @Test
+    private void checkPhraseObject(ArrayList<PhraseDO> expected, ArrayList<PhraseDO> actual) throws Exception {
+        if(expected == null)
+            throw new NullPointerException("Expected is null");
+        if(actual == null)
+            throw new NullPointerException("Actual is null");
+        if(expected.size() != actual.size())
+            throw new Exception("Expected doesn't match actual");
+        for (int i = 0; i < expected.size(); i++) {
+            if (expected.get(i).Phrase.equalsIgnoreCase(actual.get(i).Phrase)) {
+                if(expected.get(i).PhraseIteration == actual.get(i).PhraseIteration) {
+                    continue;
+                } else {
+                    throw new Exception("Expected iteration result doesn't match actual");
+                }
+            } else {
+                throw new Exception("Expected content doesn't match actual");
+            }
+        }
     }
 
     @Test
--- app/src/main/java/com/arpaul/speechtotext/dataobject/PhraseDo.java
+++ app/src/main/java/com/arpaul/speechtotext/dataobject/PhraseDo.java
@@ -1,10 +1,20 @@
 package com.arpaul.speechtotext.dataobject;
 
+import java.util.Comparator;
+
 /**
  * Created by ARPaul on 12-02-2017.
  */
 
-public class PhraseDO {
+public class PhraseDO implements Comparator<PhraseDO> {
     public String Phrase = "";
     public int PhraseIteration = 0;
+
+    @Override
+    public int compare(PhraseDO o1, PhraseDO o2) {
+        if(o1.PhraseIteration == o2.PhraseIteration)
+            return 0;
+        else
+            return 1;
+    }
 }
